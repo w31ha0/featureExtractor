82744b11f153a4efb9e5c8bd700d4bba618de4ce
Ubuntu <ubuntu@ip-172-31-28-121.ap-southeast-1.compute.internal>
2017-07-21 08:45:54 +0000

    Added intents parsing

--- constants.py
+++ constants.py
@@ -1,9 +1,19 @@
 TEMP_DIRECTORY = 'temp'
 ANDROGUARD_PATH = '../tools/androguard/'
 
+#Manifest
 MANIFEST_FILE_RAW = 'AndroidManifest.xml'
 MANIFEST_FILE_CONVERTED = 'Manifest.xml'
 
+ACTIVITY_LABEL = 'activity'
+APPLICATION_LABEL = 'application'
 PERMISSION_LABEL = 'uses-permission'
+RECEVIER_LABEL = 'receiver'
+INTENT_LABEL = 'intent-filter'
+ACTION_LABEL = 'action'
+
 PERMISSION_KEY = '{http://schemas.android.com/apk/res/android}name'
 MALICIOUS_PERMISSIONS = ['android.permission.INTERNET','android.permission.WRITE_SMS','android.permission.DISABLE_KEYGUARD']
+
+INTENT_KEY = '{http://schemas.android.com/apk/res/android}name'
+MALICIOUS_INTENTS = ['android.intent.action.BOOT_COMPLETED','android.provider.Telephony.SMS_RECEIVED','android.intent.action.UMS_CONNECTED']
--- manifestParser.py
+++ manifestParser.py
@@ -7,18 +7,35 @@ manifestPath = TEMP_DIRECTORY+'/'+MANIFEST_FILE_CONVERTED
 def parseManifest():	
 	tree = ET.parse(manifestPath)
 	root = tree.getroot()
-	parsePermissions(root)
-		
-def parsePermissions(root):
-	counter = 0
+	
+	permissionsCounter = 0
 	suspiciousPermissions = []
+	intentsCounter = 0
+	suspiciousIntents = []
+	
 	for child in root:
-		if child.tag == PERMISSION_LABEL:
+		if child.tag == PERMISSION_LABEL: #Parse permissions
 			permission = child.attrib[PERMISSION_KEY]
 			if permission in MALICIOUS_PERMISSIONS:
-				counter = counter + 1
+				permissionsCounter = permissionsCounter + 1
 				suspiciousPermissions.append(permission)
-	print "Number of suspiscious permissions is "+str(counter)
+		elif child.tag == APPLICATION_LABEL: #Parse intents
+			for innerChild in child:
+				if innerChild.tag == ACTIVITY_LABEL or innerChild.tag == RECEVIER_LABEL:
+					for innerInnerChild in innerChild:
+						if innerInnerChild.tag == INTENT_LABEL:
+							for action in innerInnerChild:
+								if action.tag == ACTION_LABEL:
+									intent = action.attrib[INTENT_KEY]
+									if intent in MALICIOUS_INTENTS:
+										intentsCounter = intentsCounter + 1
+										suspiciousIntents.append(intent)
+									
+	print "Number of suspiscious permissions is "+str(permissionsCounter)
 	print suspiciousPermissions
+	print "Number of suspiscious intents is "+str(intentsCounter)
+	print suspiciousIntents
+								
+		
 				
 	
