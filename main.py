fbef0221b56b22297324fe96986e11ca9c07d6d8
root <root@ip-172-31-28-121.ap-southeast-1.compute.internal>
2017-07-30 13:39:19 +0000

    Added parsing of network addresses

--- main.py
+++ main.py
@@ -16,6 +16,6 @@ baksmali()
 
 parseManifest()
 searchForInnerApkOrJar()
-searchSenstitiveCalls()
+parseSmali()
 
 cleanup()
--- smaliParser.py
+++ smaliParser.py
@@ -1,19 +1,56 @@
+import re
 import sys,os
 from constants import *
 
-def searchSenstitiveCalls():
+def parseSmali():
 	dir_path = os.path.dirname(os.path.realpath(__file__)) + '/' + TEMP_DIRECTORY + '/' + SMALI_PATH
-	sensitiveAPISCounter = 0
-	sensitiveAPIS = []
+	allSensitiveAPIS = []
+	allNetworkAddresses = []
 	
 	for path, subdirs, files in os.walk(dir_path):
 		for name in files:
 			if name.endswith('smali'):
 				fullpath = os.path.join(path, name)
 				content = open(fullpath,'r').read()
-				for api in SENSITIVE_APIS:
-					if api in content:
-						sensitiveAPISCounter = sensitiveAPISCounter + 1
-						sensitiveAPIS.append(fullpath + " : " + api)
+				allSensitiveAPIS.extend(searchSenstitiveCalls(content,fullpath))
+				allNetworkAddresses.extend(searchNetworkAddresses(content,fullpath))
+			else:
+				fullpath = os.path.join(path, name)
+				content = open(fullpath,'r').read()			
+				allNetworkAddresses.extend(searchNetworkAddresses(content,fullpath))				
+				
+	print "Number of sensitive APIS:" + str(len(allSensitiveAPIS)) + str(allSensitiveAPIS)
+	print "Number of network addresses:" + str(len(allNetworkAddresses)) + str(allNetworkAddresses)
+	
+
+def searchSenstitiveCalls(content,fullpath):
+	sensitiveAPIS = []
+	
+	for api in SENSITIVE_APIS:
+		if api in content:
+			record = api + " <" + fullpath + ">"
+			sensitiveAPIS.append(record)
+			
+	return sensitiveAPIS
 						
-	print "Number of sensitive API Calls: "+str(sensitiveAPISCounter)+str(sensitiveAPIS)
+
+	
+def searchNetworkAddresses(content,fullpath):
+	networkAddresses = []
+
+	ips = re.findall( r'[0-9]+(?:\.[0-9]+){3}', content )
+	for ip in ips:
+		record = ip + " <" + fullpath + ">"
+		networkAddresses.append(record)	
+		
+	return networkAddresses
+	
+def searchConstantClassNames(content,fullpath):
+	networkAddresses = []
+
+	ips = re.findall( r'[0-9]+(?:\.[0-9]+){4}', content )
+	for ip in ips:
+		record = ip + " <" + fullpath + ">"
+		networkAddresses.append(record)	
+		
+	return networkAddresses
