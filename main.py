b72ee9b03478a45b7973193cc7f07c4338d09295
root <root@ip-172-31-28-121.ap-southeast-1.compute.internal>
2017-08-21 07:22:28 +0000

    final excel saving version

--- main.py
+++ main.py
@@ -7,6 +7,47 @@ import xlwt
 from xlrd import *
 from xlutils.copy import copy
 
+def getSheetNo(sheetname):
+    try:
+        rb = open_workbook("results.csv")
+    except IOError:
+        rb = xlwt.Workbook()
+        rb.add_sheet(sheetname)
+        rb.save("results.csv")
+        return 0
+
+    sheetNo = 0
+    sheets = rb.sheet_names()
+
+    for sheet in sheets:
+        if sheet == sheetname:
+            return sheetNo
+        else:
+            sheetNo += 1
+            
+    if sheetNo >= len(sheets):
+        book = copy(rb)
+        sh = book.add_sheet(sheetname)
+        book.save("results.csv")
+        
+    return sheetNo
+    
+def getStartCol(sh):
+    startColumn = -1    
+    noOfCols = sh.ncols
+
+    for i in range(0,noOfCols):
+        startColumn = i
+        if appname == sh.col_values(i)[0]:
+            break
+        if i == (noOfCols - 1):
+            startColumn += 1
+        
+    if startColumn < 1:
+        startColumn = 1
+        
+    return startColumn
+
 parser = argparse.ArgumentParser(description='Feature Extractor')
 parser.add_argument('-i', action="store",dest="i",required=True,help="Input APK File")
 parser.add_argument('-t', action="store",dest="t",required=True,help="Type of functionality")
@@ -20,23 +61,13 @@ if len(apkname) > 29:
     appname = apkname[:30]
 else:
     appname = apkname
-
-startColumn = -1    
     
-try:
-    rb = open_workbook("results.csv")
-    book = copy(rb)
-    sh = rb.sheet_by_name(sheetname)
-    startColumn = sh.ncols
-except IOError:
-    rb = xlwt.Workbook()
-    sh = rb.add_sheet(sheetname)
-except XLRDError:
-    sh = rb.add_sheet(sheetname)
-    
-if startColumn == -1:
-    startColumn = 1
-
+sheetNo = getSheetNo(sheetname)
+rb = open_workbook("results.csv")
+sh = rb.sheet_by_name(sheetname)
+startColumn = getStartCol(sh)
+book = copy(rb)
+sh = book.get_sheet(sheetNo)
 sh.write(0,startColumn,appname)    
     
 disassemble(apkfile)
